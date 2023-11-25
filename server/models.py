8d3271f0fd79ff487cf8c7203d422e4e3a4f29ad
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-25 07:28:59 +0300

    first commit

---
+++ server/models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Products(db.Model):
+    __tablename__='products'
+    id = db.Column(db.Integer, primary_key=True)
+    product_name=db.Column(db.String)
+    cost=db.Column(db.Integer)
+    image=db.Column(db.String)
+
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name=db.Column(db.String)
+    email=db.Column(db.String)
+    phonenumber=db.Column(db.String)
+    message= db.Column(db.String)
+    service = db.Column(db.String)
+    vehicle_make= db.Column(db.String)
+    vehicle_model= db.Column(db.Integer)
+    appointment_date=db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+    time_frame= db.Column(db.String)
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name=db.Column(db.String)
+    email=db.Column(db.String)
+    phonenumber=db.Column(db.String)
+    message= db.Column(db.String)
+    vehicle_make= db.Column(db.String)
+    vehicle_no= db.Column(db.Integer)
+    service = db.Column(db.String)
+    appointment_date= db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+
+
+
