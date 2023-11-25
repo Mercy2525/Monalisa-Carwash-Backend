f61e46d85dcf43eda5b2ed6ddd84c342db832a8f
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-25 07:55:48 +0300

    feature: added flask restful

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db 
+from models import db, Appointment, Product, Feedback 
+
+from flask_restful import Api, Resource
+
+
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
@@ -8,3 +12,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 migrate=Migrate(app,db)
 db.init_app(app)
+
+api=Api(app)
