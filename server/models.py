d68f464a6322cae8e7b4e8776371098c4ee3a999
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-25 07:54:07 +0300

    refactor:changes some table column and table names

--- server/models.py
+++ server/models.py
@@ -2,16 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Products(db.Model):
+class Product(db.Model):
     __tablename__='products'
+
     id = db.Column(db.Integer, primary_key=True)
+
     product_name=db.Column(db.String)
     cost=db.Column(db.Integer)
     image=db.Column(db.String)
 
 
 class Appointment(db.Model):
+    __tablename__='appointments'
+
     id = db.Column(db.Integer, primary_key=True)
+
     full_name=db.Column(db.String)
     email=db.Column(db.String)
     phonenumber=db.Column(db.String)
@@ -19,17 +24,21 @@ class Appointment(db.Model):
     service = db.Column(db.String)
     vehicle_make= db.Column(db.String)
     vehicle_model= db.Column(db.Integer)
-    appointment_date=db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+    appointment_date=db.Column(db.DateTime, default=db.func.current_timestamp())
     time_frame= db.Column(db.String)
 
 class Feedback(db.Model):
+    __tablename__='feedbacks'
+
     id = db.Column(db.Integer, primary_key=True)
+
     full_name=db.Column(db.String)
     email=db.Column(db.String)
     phonenumber=db.Column(db.String)
     message= db.Column(db.String)
     vehicle_make= db.Column(db.String)
     vehicle_no= db.Column(db.Integer)
+    
     service = db.Column(db.String)
     appointment_date= db.Column(db.DateTime, onupdate=db.func.current_timestamp())
 
